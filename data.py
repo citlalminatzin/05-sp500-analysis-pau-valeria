11d99cb752af91a07d85cda2e7c8b5318f85599e
Alba420p <albapaulina150@gmail.com>
2026-03-12 14:00:25 -0600

    Update data.py

--- data.py
+++ data.py
@@ -8,9 +8,9 @@ def get_stock_data(ticker:str = "^GSPC", years: int = 5, path:str = "data/sp500.
     stock = Ticker(ticker).history(period = t)
     stock.to_csv(path, encoding = 'utf-8')
 
-def read_data(path="data/pescados.csv"):
+def read_data(path="data/sp500.csv"):
     """Lee los datos de un csv y te los devuelve"""
-    df = pd.read_csv("data/pescados.csv")
+    df = pd.read_data("data/sp500.csv")
     print(df.head())
     ...
 
