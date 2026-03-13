75498171d6d048a7e896cbbadc5a8fa1b49c1b2a
Alba420p <albapaulina150@gmail.com>
2026-03-12 19:18:24 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from yfinance import Ticker
+import pandas as pd
+import matplotlib.pyplot as plt 
 
 def get_stock_data(ticker:str = "^GSPC", years: int = 5, path:str = "data/sp500.csv"):
     """Gets historic data for a ticker from yfinance API"""
@@ -10,12 +12,21 @@ def get_stock_data(ticker:str = "^GSPC", years: int = 5, path:str = "data/sp500.
 
 def read_data(path="data/sp500.csv"):
     """Lee los datos de un csv y te los devuelve"""
-    df = pd.read_data("data/sp500.csv")
-    print(df.head())
-    ...
+    df = pd.read_csv(path)
+    df["Date"] = pd.to_datetime(df["Date"]) #me convierte las fechas a formato Date
+    return df
 
-def main():
-    get_stock_data()
+def stock_plot(df):
+    """Grafica el precio de cierre"""
+    df = df.sort_values("Date")
 
+    plt.figure(figsize=(10,5))
+    plt.plot(df["Date"], df["Close"])
+    plt.title("Precio del S&P 500 (últimos 5 años)")
+    plt.xlabel("Fecha")
+    plt.ylabel("Precio de cierre")
+    plt.grid(True)
+    plt.show()
+    
 if __name__ == "__main__":
     main()
